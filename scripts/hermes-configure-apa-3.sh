bc78d299e96afa62730dbcadc87a38abe25d80b1
Alessandro Thea <alessandro.thea@cern.ch>
2023-05-26 11:38:15 +0200

    Fixing connection file

--- scripts/hermes-configure-apa-3.sh
+++ scripts/hermes-configure-apa-3.sh
@@ -15,14 +15,14 @@ for slot in $(seq 1 5); do
 done
 
 wiblis=$(seq 1 5)
-# wiblis="1"
+wiblis="1"
 for slot in ${wiblis}; do
     for link in 0 1; do
         wib=${crate}0${slot}
         hermesbutler.py -d np04-wib-${wib} enable -l $link --dis 
         hermesbutler.py -d np04-wib-${wib} udp-config -l $link np04-wib-${wib}-d${link} ${target_nic}
         hermesbutler.py -d np04-wib-${wib} mux-config -l $link ${det} ${crate} $slot
-        hermesbutler.py -d np04-wib-${wib} fakesrc-config -l $link -n 0
+        hermesbutler.py -d np04-wib-${wib} fakesrc-config -l $link -n 4
         hermesbutler.py -d np04-wib-${wib} enable -l $link --en
         hermesbutler.py -d np04-wib-${wib} stats -l $link
     done
--- scripts/hermes-reboot-wibs.py
+++ scripts/hermes-reboot-wibs.py
@@ -1,12 +1,29 @@
+#!/usr/bin/env python
+
+
+import click
 from pexpect import pxssh
 import getpass
 
-password = getpass.getpass('password: ')
+@click.command()
+@click.option('-n', '--dry-run', is_flag=True)
+@click.argument("crate", type=click.IntRange(3,5))
+def cli(dry_run, crate):
+    """"""
+    password = getpass.getpass('Password: ')
 
-for i in range(1,6):
-    s = pxssh.pxssh()
-    s.login(f'np04-wib-30{i}', 'root', password)
-    s.sendline('uptime')   # run a command
-    s.prompt()             # match the prompt
-    print(s.before)        # print everything before the prompt.
-    s.logout()
+    for i in range(1,6):
+        hostname = f'np04-wib-{crate}0{i}'
+        s = pxssh.pxssh()
+        s.login(hostname, 'root', password)
+        s.sendline('uptime')   # run a command
+        s.prompt()             # match the prompt
+        print(hostname, s.before.decode('utf-8'))        # print everything before the prompt.
+        if not dry_run:
+            s.sendline('reboot')   # run a command
+            s.prompt()
+            print(hostname, s.before.decode('utf-8'))        # print everything before the prompt.
+        s.logout()
+
+if __name__ == "__main__":
+    cli()

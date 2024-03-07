515cf22f1b536709a4b2e6e35ab74fa18118a816
Gordon Crone <g.crone@ucl.ac.uk>
2024-03-07 14:38:35 +0000

    HermesLinkconf id renamed to link_id. Get stream_params from source in cast to EthStreamParameters

--- plugins/HermesController.hpp
+++ plugins/HermesController.hpp
@@ -42,7 +42,7 @@ ERS_DECLARE_ISSUE(hermesmodules,
 
 ERS_DECLARE_ISSUE(hermesmodules,
                   InvalidSourceStream,
-                  "Stream configuration for " << what << "is not an ethernet stream",
+                  "Stream configuration for " << what << " is not an ethernet stream",
                   ((std::string)what)
                   );
 
